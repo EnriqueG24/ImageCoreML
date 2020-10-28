18272733d3abbee6f398be1aae1d9905822afa2f
Enrique Gongora <EGTech24@gmail.com>
2020-10-27 19:51:18 -0700

    Created ImagePicker Delegates and necessary methods to add images

--- ImageCoreML/ImageCoreML/ViewController.swift
+++ ImageCoreML/ImageCoreML/ViewController.swift
@@ -11,8 +11,12 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: - Properties
+    let imagePicker = UIImagePickerController()
+    
     // MARK: - IBActions
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
     }
     
     // MARK: - IBOutlets
@@ -21,7 +25,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+    }
+    
+    // MARK: - Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let userSelectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imageView.image = userSelectedImage
     }
 }
-
